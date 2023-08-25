1348c1f7682df74b0d3444eece43f91445f6233f
DKhorkov <dkhorkov.dev@gmail.com>
2023-08-25 10:36:48 +0300

    Added tests for answers structure

---
+++ src/answers/answers_test.go
@@ -0,0 +1,33 @@
+package answers
+
+import (
+	"reflect"
+	"testing"
+)
+
+
+var answers Answers = Answers{
+	Yes: AnswerInfo{
+		Number: 1, 
+		Description: "Yes",
+		}, 
+	No: AnswerInfo{
+		Number: 2, 
+		Description: "No",
+		}, 
+}
+
+
+func TestAnswersToSlice(t* testing.T) {
+	answers_slice := answers.ToSlice()
+	answers_slice_len := len(answers_slice)
+	wanted_len := 2
+
+	if wanted_len != answers_slice_len {
+		t.Fatalf("Operations_slice length is incorrect: %v wanted, but %v received", wanted_len, answers_slice_len)
+	}
+
+	if reflect.TypeOf(answers_slice) != reflect.TypeOf([]AnswerInfo{}) {
+		t.Fatalf("%v is not a slice of type OperationInfo", answers_slice)
+	}
+}
